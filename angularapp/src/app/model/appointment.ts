d96eac6b59e6489c5ec9b12e58d4b80ec4478b6d
azhaguvenkadesh <119848180+azhaguvenkadesh@users.noreply.github.com>
2024-08-29 10:37:02 +0530

    Delete angularapp directory

--- angularapp/src/app/model/appointment.ts
+++
@@ -1,8 +0,0 @@
-export interface Appointment {
-    id?: number;
-    patientName: string;
-    doctorName: string;
-    specialization: string;
-    appointmentDate: string;
-    timeSlot: string;
-  }  
